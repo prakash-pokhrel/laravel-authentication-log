9201b48377bf62808670d3214a081bc5bbc01826
Prakash Pokhrel <prakash@zentreasury.com>
2018-11-28 09:29:13 +0200

    Only update authentication log if user exists on logout

--- src/Listeners/LogSuccessfulLogout.php
+++ src/Listeners/LogSuccessfulLogout.php
@@ -11,45 +11,44 @@ class LogSuccessfulLogout
 {
     /**
      * The request.
-     *
      * @var \Illuminate\Http\Request
      */
     public $request;
 
     /**
      * Create the event listener.
-     *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return void
      */
     public function __construct(Request $request)
     {
-        $this->request = $request;
+        $this -> request = $request;
     }
 
     /**
      * Handle the event.
-     *
-     * @param  Logout  $event
+     * @param  Logout $event
      * @return void
      */
     public function handle(Logout $event)
     {
-        $user = $event->user;
-        $ip = $this->request->ip();
-        $userAgent = $this->request->userAgent();
-        if($user)
-            $authenticationLog = $user->authentications()->whereIpAddress($ip)->whereUserAgent($userAgent)->first();
+        $user = $event -> user;
+        $ip = $this -> request -> ip();
+        $userAgent = $this -> request -> userAgent();
+        if($user) {
+            $authenticationLog = $user -> authentications() -> whereIpAddress($ip) -> whereUserAgent($userAgent) -> first();
 
-        if (! $authenticationLog) {
-            $authenticationLog = new AuthenticationLog([
-                'ip_address' => $ip,
-                'user_agent' => $userAgent,
-            ]);
+            if($authenticationLog) {
+                $authenticationLog = new AuthenticationLog([
+                    'ip_address' => $ip,
+                    'user_agent' => $userAgent,
+                ]);
+            }
+
+            $authenticationLog -> logout_at = Carbon ::now();
+
+            $user -> authentications() -> save($authenticationLog);
         }
 
-        $authenticationLog->logout_at = Carbon::now();
-
-        $user->authentications()->save($authenticationLog);
     }
 }
